40d63f1ca99df56ff631cd41f099897d110f947a
varun <59528216+varun1048@users.noreply.github.com>
2021-01-03 19:15:38 +0530

    a

---
+++ css/src/app.jsx
@@ -0,0 +1,18 @@
+import "./app.css"
+export  default function App(){
+    return <div>
+        <div>
+            AA
+            <img src="C:\Users\user\Pictures\lock\thala.jpg" alt="img"/>
+        </div>
+
+
+
+
+        <div className="parent">
+            <div className="child">
+
+            </div>
+        </div>
+    </div>
+}
